897d050d067ab76f60f2941412121c5be77aa28e
s71v10 <stef.brosteanu@gmail.com>
2013-10-11 14:30:27 +0300

    added subfolders for admin/ frontend added .htaccess file. added config.php file

---
+++ myFramework/config/config.php
@@ -0,0 +1,18 @@
+<?php
+//Folder paths
+define('LIBRARY_PATH', 'library/');
+define('CONTROLLER_PATH','controller/');
+define('MODEL_PATH', 'model/');
+define('TEMPLATES_PATH','template/');
+define('VIEW_PATH','view/');
+
+
+//Database parameters
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','1234');
+define('DB_NAME','test_db');
+
+//Site parameters
+define('SITE_URL','http://localhost/framework/myFramework');
+define('TEMPLATES_URL','http://localhost/framework/myFramework/template');
