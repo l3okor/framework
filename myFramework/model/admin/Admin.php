257e55b2d54256c94f2ac76b9fc4b230ca55cfbd
s71v10 <stef.brosteanu@gmail.com>
2013-10-21 13:40:45 +0300

    added admin model.

--- myFramework/model/admin/Admin.php
+++ myFramework/model/admin/Admin.php
@@ -1,1 +1,21 @@
 <?php
+
+class Admin extends Model
+{
+	public function getAdmin($admin, $pass )
+	{
+		$this->db->select()
+		->from('admin', array('id', 'name', 'password'))
+		->where('name = ?', $admin)
+		->where('password = ?', $pass);
+		return $this->db->fetchAll();
+	}
+
+	public function getRegAdmin($admin)
+	{
+		$this->db->select()
+		->from('admin', array('id', 'name'))
+		->where('name = ?', $admin);
+		return $this->db->fetchAll();
+	}
+}
